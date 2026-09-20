1bbeddce1b30accf22d61b320e877281e995d287
Matheus <273137@unifio.edu.br>
2026-09-20 20:27:21 +0000

    Primeiro endpoint funcionando

---
+++ src/test/java/com/matheus/ecommerce_backend/EcommerceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.ecommerce_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
